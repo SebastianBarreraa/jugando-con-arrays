13c10dfc7393c0aaa4b0a77ee0b7d8922584534c
SebastianBarreraa <141358797+SebastianBarreraa@users.noreply.github.com>
2023-11-29 12:45:56 -0300

    initial commit

---
+++ Evento.js
@@ -0,0 +1,58 @@
+// Array extraño
+const strangeArray = [
+    'Zero',
+    function () {
+      alert('Hola soy una función en un array')
+    },
+    22,
+    null,
+    'Go lang',
+    undefined,
+    'Cobol',
+    'I\'m programmer',
+    -2000,
+    'Hello world',
+    `One is ${1}`,
+    { name: 'Info', lastname: 'last info' },
+    () => true,
+    function showNumbers() {
+      return '1, 2, 3, 4'
+    },
+    'Another String',
+    ['Hola mundo!'],
+    'b is a letter',
+    'JavaScript',
+  ]
+  
+  // Función que recibe por parámetro un array y muestra sus elementos en pantalla
+  function showList(array) {
+    const container = document.getElementById('list')
+    container.innerHTML = ''
+    // Más info de forEach => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+    array.forEach((element) => {
+      const li = document.createElement('li')
+      li.appendChild(document.createTextNode(element))
+      container.appendChild(li)
+    })
+  }
+  
+  // Escribe tu solución aquí
+  document.addEventListener("DOMContentLoaded", (event) => {
+    const filteredStrings = strangeArray
+    .filter(item => typeof item === 'string')
+    .sort((a, b) => a.localeCompare(b, 'en', {sensitivity: 'base'}));
+  
+    displayItems(filteredStrings);
+  });
+  
+  function displayItems(items) {
+    const container = document.getElementById('list');
+    container.innerHTML = '';
+  
+    items.forEach((item) => {
+      const listItem = document.createElement('li');
+      listItem.appendChild(document.createTextNode(item));
+      container.appendChild(listItem);
+    });
+  }
+  
